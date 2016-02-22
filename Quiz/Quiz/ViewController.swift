8f1506b8140b0e542ace681cc351d968d9542121
Jeff Johnson <jfjohnson@solstice-consulting.com>
2016-02-22 16:24:29 -0600

    finish animations for quiz

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var currentQuestionLabelXConstraint: NSLayoutConstraint!
+    @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabelXConstraint: NSLayoutConstraint!
     @IBOutlet var answerLabel: UILabel!
     
     let questions: [String] = ["From what is cognac made?", "What is 7+7?", "What is the capital of Vermont?"]
@@ -25,8 +28,10 @@ class ViewController: UIViewController {
         }
         
         let question: String = questions[currentQuestionIndex];
-        questionLabel.text = question;
+        nextQuestionLabel.text = question
         answerLabel.text = "???"
+        
+        animateLabelTransitions()
     }
     
     @IBAction func showAnswer(sender: AnyObject){
@@ -37,6 +42,43 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         questionLabel.text = questions[currentQuestionIndex]
+        updateOffScreenLabel()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        nextQuestionLabel.alpha = 0
+    }
+    
+    func updateOffScreenLabel(){
+        let screenWidth = view.frame.width
+        nextQuestionLabelXConstraint.constant = -screenWidth
+    }
+    
+    func animateLabelTransitions(){
+        view.layoutIfNeeded()
+        
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelXConstraint.constant = 0
+        self.currentQuestionLabelXConstraint.constant += screenWidth
+        
+        UIView.animateWithDuration(0.5,
+            delay: 0,
+            usingSpringWithDamping: 0.5,
+            initialSpringVelocity: 0.75,
+            options: [UIViewAnimationOptions.CurveEaseOut],
+            animations: {
+                self.questionLabel.alpha = 0
+                self.nextQuestionLabel.alpha = 1
+                self.view.layoutIfNeeded()
+            },
+            completion: { _ in
+                swap (&self.questionLabel, &self.nextQuestionLabel)
+                swap (&self.currentQuestionLabelXConstraint, &self.nextQuestionLabelXConstraint)
+                self.updateOffScreenLabel()
+            }
+        )
     }
 }
 
