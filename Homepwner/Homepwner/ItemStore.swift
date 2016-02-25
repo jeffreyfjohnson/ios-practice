7bc06c49b2ef401462d8fa3a612c67d0e21e1419
Jeff Johnson <jfjohnson@solstice-consulting.com>
2016-02-25 17:34:18 -0600

    implement table view and controller

---
+++ Homepwner/Homepwner/ItemStore.swift
@@ -0,0 +1,27 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Solstice Loaner on 2/25/16.
+//  Copyright © 2016 Solstice Loaner. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var items = [Item]()
+    
+    init() {
+        for _ in 0..<100{
+            createItem()
+        }
+    }
+    
+    func createItem() -> Item {
+        let newItem = Item(random: true)
+        
+        items.append(newItem)
+        
+        return newItem
+    }
+}
---
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Solstice Loaner on 2/25/16.
+//  Copyright © 2016 Solstice Loaner. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController{
+    
+    var itemStore: ItemStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.items.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) ->UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
+        
+        let item = itemStore.items[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = String(item.valueInDollars)
+        
+        return cell
+    }
+}
