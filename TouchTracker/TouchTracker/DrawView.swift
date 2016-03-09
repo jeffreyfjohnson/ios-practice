a27dc442d2c752fe445a117e031c262f649e3f46
Jeff Johnson <jfjohnson@solstice-consulting.com>
2016-03-09 08:35:55 -0600

    finished circles

--- TouchTracker/TouchTracker/DrawView.swift
+++ TouchTracker/TouchTracker/DrawView.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class DrawView: UIView{
     
-    var currentLines = [NSValue: Line]()
+    var currentLine: Line?
+    var currentTouches = [NSValue: CGPoint]()
     var finishedLines = [Line]()
+    var finishedCircles = [(CGPoint, CGPoint)]()
+    
+    //MARK: Inspectables
     
     @IBInspectable var finishedLineColor: UIColor = UIColor.blackColor(){
         didSet{
@@ -31,6 +35,18 @@ class DrawView: UIView{
         }
     }
     
+    @IBInspectable var currentCircleColor: UIColor = UIColor.greenColor(){
+        didSet{
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var finishedCircleColor: UIColor = UIColor.grayColor(){
+        didSet{
+            setNeedsDisplay()
+        }
+    }
+    
     @IBInspectable var lineThickness: CGFloat = 10{
         didSet{
             setNeedsDisplay()
@@ -41,10 +57,18 @@ class DrawView: UIView{
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
         for touch in touches {
-            let newLine = Line(begin: touch.locationInView(self), end: touch.locationInView(self));
-            
-            currentLines[valueForTouch(touch)] = newLine
+            currentTouches[valueForTouch(touch)] = touch.locationInView(self)
         }
+        
+        if currentTouches.count == 1{
+            if let touch = currentTouches.values.first{
+                currentLine = Line(begin: touch, end: touch);
+            }
+        }
+        else {
+            currentLine = nil
+        }
+
         
         setNeedsDisplay()
     }
@@ -52,7 +76,12 @@ class DrawView: UIView{
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
         for touch in touches {
-             currentLines[valueForTouch(touch)]?.end =  touch.locationInView(self)
+             currentTouches[valueForTouch(touch)] = touch.locationInView(self)
+        }
+        
+        if var line = currentLine, let touch = currentTouches.values.first {
+            line.end = touch
+            currentLine = line
         }
        
         setNeedsDisplay()
@@ -60,21 +89,29 @@ class DrawView: UIView{
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
-        for touch in touches{
+        if var line = currentLine, let touch = touches.first {
+            line.end = touch.locationInView(self)
+            finishedLines.append(line)
+            currentTouches.removeValueForKey(valueForTouch(touch))
+            currentLine = nil
+        }
+        else {
+            var points = [CGPoint]()
             
-            if var current = currentLines[valueForTouch(touch)]{
-                current.end = touch.locationInView(self)
-                finishedLines.append(current)
-                currentLines.removeValueForKey(valueForTouch(touch))
+            for touch in touches {
+                points.append(touch.locationInView(self))
             }
             
+            finishedCircles.append((points[0], points[1]))
+            currentTouches.removeAll()
         }
-        
+
         setNeedsDisplay()
     }
     
     override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
-        currentLines.removeAll()
+        currentTouches.removeAll()
+        currentLine = nil
         setNeedsDisplay()
     }
     
@@ -93,6 +130,13 @@ class DrawView: UIView{
         path.stroke()
     }
     
+    func strokeCircle(pointA: CGPoint, pointB: CGPoint){
+        let path = UIBezierPath(arcCenter: midpoint(pointA, pointB), radius: distance(pointA, pointB)/2.0, startAngle: 0, endAngle: CGFloat(2.0 * M_PI), clockwise: true)
+        
+        path.lineWidth = lineThickness
+        path.stroke()
+    }
+    
     override func drawRect(rect: CGRect) {
         finishedLineColor.setStroke();
         
@@ -100,22 +144,44 @@ class DrawView: UIView{
             strokeLine(line)
         }
         
+        finishedCircleColor.setStroke()
+        
+        for (pointA, pointB) in finishedCircles {
+            strokeCircle(pointA, pointB: pointB)
+        }
+        
         currentLineColor.setStroke()
         
-        for (_ , line) in currentLines {
+        if let line = currentLine {
             let height = abs(line.end.y - line.begin.y)
             let length = abs(line.end.x - line.begin.x)
-            print("length = \(length)")
-            print("height = \(height)")
             let angle = atan(Double(height) / Double(length))
-            print(angle)
-            print("**********")
             
-            interpolateColor(currentLineColor, b: currentLineColorInterpolate, t: CGFloat(abs(angle))).setStroke()
+            interpolateColor(currentLineColor, b: currentLineColorInterpolate, t: CGFloat(angle / (M_PI / 2.0))).setStroke()
             
             strokeLine(line)
         }
+        else{
+            var points = [CGPoint]()
+            
+            for touch in currentTouches.values {
+                points.append(touch)
+            }
+            
+            if (points.count >= 2){
+                strokeCircle(points[0], pointB: points[1])
+            }
+        }
         
+        
+    }
+    
+    func midpoint(a: CGPoint, _ b: CGPoint) -> CGPoint{
+        return CGPoint(x: (a.x+b.x)/CGFloat(2.0), y: (a.y+b.y)/CGFloat(2.0));
+    }
+    
+    func distance(a: CGPoint, _ b: CGPoint) -> CGFloat{
+        return CGFloat(sqrt(pow((a.x - b.x), 2) + pow((a.y - b.y), 2)))
     }
     
     func interpolateColor(a: UIColor, b: UIColor, t: CGFloat) -> UIColor{
